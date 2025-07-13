aab5957023e87a5018c919720b4cfe7959d525e4
HeyBail <Bill331333@gmail.com>
2025-07-12 19:25:37 -0500

    feature(flight plan) adds getting flight plan for student and semester from grad and updates generate flight plan to handle semesters and students having multiple flight plans
    Changes
    	Implements the back end changes for flight plan checking on login. Will check if a student has a flight plan for their current semester from graduation. Will generate a flight plan for them in the current semester or the next one if they do not have one. Will create a new semester if the current one or the next one does not exist.
    Todo
    	Check if the newest flight plan's semester has passed. If so, decrement the students semester's from graduation and generate a new flight plan.

--- app/sequelizeUtils/semester.js
+++ app/sequelizeUtils/semester.js
@@ -28,4 +28,89 @@ exports.getCurrentSemester = async () => {
   });
 };
 
+exports.createNextExpectedSemester = async () => {
+  const nextExpectedSemesterData = generateNextExpectedSemesterData();
+
+  return await Semester.create(nextExpectedSemesterData);
+}
+
+exports.getNextExpectedSemester = async () => {
+  const dayWithinNextSemester = getNextSemesterStartPlusOneDay();
+
+  return await Semester.findOne({
+    where:{
+      startDate: {
+        [Op.lte]: dayWithinNextSemester,
+      },
+      endDate: {
+        [Op.gte]: dayWithinNextSemester,
+      },
+    }
+  })
+}
+
+// does not do summer terms
+// gets the expected next semester start day plus one given the current date, expects fall start to be 08/01, and spring to be 01/15 
+function getNextSemesterStartPlusOneDay() {
+  const now = new Date();
+  const year = now.getFullYear();
+
+  const springStart = new Date(`${year}-01-15T01:00:00`);
+  const fallStart = new Date(`${year}-08-01T01:00:00`);
+
+  let nextStart;
+
+  if (now < springStart) {
+    nextStart = springStart;
+  } else if (now < fallStart) {
+    nextStart = fallStart;
+  } else {
+    nextStart = new Date(`${year + 1}-01-15T01:00:00`);
+  }
+
+  const oneDayMs = 24 * 60 * 60 * 1000;
+  const result = new Date(nextStart.getTime() + oneDayMs);
+
+  return result;
+}
+
+// does not do summer terms
+// expects fall start to be 08/01, and spring to be 01/15
+function generateNextExpectedSemesterData() {
+  const now = new Date();
+  const year = now.getFullYear();
+
+  const springStart = new Date(`${year}-01-15T01:00:00`);
+  const fallStart = new Date(`${year}-08-01T01:00:00`);
+
+  const springEnd = new Date(`${year}-04-28T01:00:00`)
+  const fallEnd = new Date(`${year}-12-15T01:00:00`)
+
+  let nextTerm, nextYear, startDate, endDate;
+
+  if (now < springStart) {
+    nextTerm = 'spring';
+    nextYear = year;
+    startDate = springStart;
+    endDate = springEnd;
+  } else if (now < fallStart) {
+    nextTerm = 'fall';
+    nextYear = year;
+    startDate = fallStart;
+    endDate = fallEnd;
+  } else {
+    nextTerm = 'spring';
+    nextYear = year + 1;
+    startDate = new Date(`${nextYear}-01-15T01:00:00`);
+    endDate = new Date(`${nextYear}-04-28T01:00:00`);
+  }
+
+  return {
+    term: nextTerm,
+    year: nextYear,
+    startDate,
+    endDate,
+  };
+}
+
 export default exports;
